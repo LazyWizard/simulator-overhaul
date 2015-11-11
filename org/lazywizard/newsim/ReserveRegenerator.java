0036255f6873e0021d335b4c69b7d8d3db0d873d
Robert Maguire <LazyWizard@users.noreply.github.com>
2015-11-10 22:45:03 -0600

    Removed unused files

--- org/lazywizard/newsim/ReserveRegenerator.java
+++
@@ -1,8 +0,0 @@
-package org.lazywizard.newsim;
-
-import com.fs.starfarer.api.combat.EveryFrameCombatPlugin;
-
-public interface ReserveRegenerator extends EveryFrameCombatPlugin
-{
-    public void checkReserves();
-}
